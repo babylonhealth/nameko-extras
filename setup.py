43443c13ae960459ace06d8b7e72f5fd4ecad6bc
Richard O'Dwyer <richard@richard.do>
2017-09-01 12:26:58 +0100

    Preparing release 0.1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='nameko-extras',
-    version='0.1.0.dev0',
+    version='0.1.0',
     packages=find_packages('src', exclude=('tests',)),
     package_dir={'': 'src'},
     include_package_data=True,
